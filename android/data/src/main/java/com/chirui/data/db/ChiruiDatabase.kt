a953c311383d8993fd926f82541faf85587c1cdc
ItsDresch <65150772+swolem12@users.noreply.github.com>
2025-11-26 21:34:29 +0900

    Add entities and DAOs to ChiruiDatabase

--- android/data/src/main/java/com/chirui/data/db/ChiruiDatabase.kt
+++ android/data/src/main/java/com/chirui/data/db/ChiruiDatabase.kt
@@ -2,6 +2,22 @@ package com.chirui.data.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.chirui.data.db.dao.LibraryMangaDao
+import com.chirui.data.db.dao.ReadingHistoryDao
+import com.chirui.data.db.entity.LibraryMangaEntity
+import com.chirui.data.db.entity.ReadingHistoryEntity
 
-@Database(entities = [], version = 1, exportSchema = false)
-abstract class ChiruiDatabase : RoomDatabase()
+@Database(
+    entities = [
+        LibraryMangaEntity::class,
+        ReadingHistoryEntity::class,
+    ],
+    version = 1,
+    exportSchema = false,
+)
+abstract class ChiruiDatabase : RoomDatabase() {
+
+    abstract fun libraryMangaDao(): LibraryMangaDao
+
+    abstract fun readingHistoryDao(): ReadingHistoryDao
+}
